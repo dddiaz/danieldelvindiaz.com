e9ac185b101a564718b2a9f31ca04955981e6ee8
Daniel Diaz <daniel.delvin.diaz@gmail.com>
2016-02-22 21:01:07 -0800

    before svg changes

--- js/bg.js
+++ js/bg.js
@@ -1,63 +1,77 @@
 $(document).ready(function(){
     //console.log("Testing");
+    //RETURN for testing
+    //return;
 
     $.getJSON("https://uxlmtxq0vb.execute-api.us-east-1.amazonaws.com/test/last-bg-reading", function(data) {
-        // Get the element with id summary and set the inner text to the result.
-        //$('#BG-Data').text(data);
-        // console.log(data);
-        // for (var key in data) {
-        //   if (data.hasOwnProperty(key)) {
-        //     console.log(key + " -> " + data[key]);
-        //   }
-        // }
-        //$('#sgv').text(data['sgv']);
-        //$('#trend').text(data['trend']);
         var bg = data['sgv'];
         var trend = data['trend'];
         var direction = data['direction'];
-        _showBGInfo(bg);
-        _showBGTrend(trend,direction);
+        _setBGInfoText(bg,trend,direction);
+        _adjustBGInfoDonut(bg);
+        //$('#bg-donut').css("background-image","url('images/sailing-compressed.jpg')");
     });
 
     //Function to adjust donut
-    var _showBGInfo = function(bg){
+    var _adjustBGInfoDonut = function(bg){
       if (bg){ //bg is not null or empty
         if (bg < 80){
           $('.donut').addClass('one-quarter-filled');
-          $('#BGText').text("Currently my Blood Glucose is below average.");
         } else if ( bg > 180){
           $('.donut').addClass('three-quarter-filled');
-          $('#BGText').text("Currently my Blood Glucose is above average.");
         } else {
           //Normal
           $('.donut').addClass('half-filled');
-          $('#BGText').text("Currently my Blood Glucose is in an average range.");
         }
       }
     }
 
-    var _showBGTrend = function(trend,direction){
+    var _setBGInfoText = function(bg,trend,direction){
+        $('#BGText').text(_generateBGText(bg,trend,direction));
+    }
+
+    var _generateBGTrendText = function(trend,direction){
         var dir = _getDirection(direction)
         if (trend){ //bg is not null or empty
-            $('#BGTrendText').text("Currently my Blood Glucose is trending "
+            return (" is trending "
                 + dir + " by " + trend + " points");
         }
     }
 
-    var _getDirection = function(direction){
-        if (!direction) {
-            return "Down"
+    var _generateBGText = function(bg,trend,direction){
+        var dir = _getDirection(direction)
+        var trendText = " and no current trend info.";
+        var bgText = "";
+        var result = "No Last Blood Glucose Reading...";
+        if (trend && direction){ //bg is not null or empty
+            trendText = " and is trending "
+                + dir + " by " + trend + " points.";
         }
-        if (direction.lastIndexOf("Down") == -1){
-            return "Up"
-        } else {
-            return "Down"
+        if (bg){ //bg is not null or empty
+          if (bg < 80){
+              bgText = "Currently my Blood Glucose is below average";
+          } else if ( bg > 180){
+              bgText = "Currently my Blood Glucose is above average";
+          } else {
+            //Normal
+            bgText = "Currently my Blood Glucose is average";
+          }
         }
+
+        if (bgText){
+            return bgText + trendText;
+        }
+
     }
 
-    // function lastBGReadingCallback (data){
-    //     var BGObj = jQuery.parseJSON(data);
-    //     $('#sgv').text(BGObj.sgv);
-    // }
-
+    var _getDirection = function(direction){
+        if (!direction) {
+            return "down"
+        }
+        if (direction.lastIndexOf("Down") == -1){
+            return "up"
+        } else {
+            return "down"
+        }
+    }
 })
