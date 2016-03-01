43691519da86e49906609ae98aa9604434f10c21
Daniel Diaz <daniel.delvin.diaz@gmail.com>
2016-02-29 19:51:43 -0800

    form quick fix

--- js/formspree.js
+++ js/formspree.js
@@ -1,13 +1,14 @@
 
 $(function(){
     //Formspree Setup
-    var $contactForm = $('#contact-form');
+    //var $contactForm = $('#contact-form');
+    var $contactForm = $('#contact-submit-btn');
     $contactForm.click(function(e) {
         e.preventDefault();
         $.ajax({
             url: '//formspree.io/daniel.delvin.diaz@gmail.com',
             method: 'POST',
-            data: $(this).serialize(),
+            data: $('#contact-form').serialize(),
             dataType: 'json',
             beforeSend: function() {
             },
@@ -15,10 +16,10 @@ $(function(){
                 //$contactForm.find('#contact-form').hide();
                 //$(this).closest('form').find("input[type=text], textarea").val("");
                 $("#contact-submit-btn").hide();
-                $contactForm.append('<div class="">Message sent! I will get back to you shortly!</div>');
+                $('#contact-form').append('<div class="">Message sent! I will get back to you shortly!</div>');
             },
             error: function(err) {
-                $contactForm.append('<div class=""> Dang it, there was an error. Make sure you filled out the fields and please try again.</div>');
+                $('#contact-form')append('<div class=""> Dang it, there was an error. Make sure you filled out the fields and please try again.</div>');
             }
         });
     });
