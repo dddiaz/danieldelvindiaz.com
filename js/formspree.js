b93aa2b370c40fa4e40a75ad9cb3c77273591055
Daniel Diaz <daniel.delvin.diaz@gmail.com>
2016-02-29 20:15:02 -0800

    adjusting button

--- js/formspree.js
+++ js/formspree.js
@@ -1,14 +1,14 @@
 
 $(function(){
     //Formspree Setup
-    //var $contactForm = $('#contact-form');
-    var $contactForm = $('#contact-submit-btn');
-    $contactForm.click(function(e) {
+    var $contactForm = $('#contact-form');
+    $contactForm.submit(function(e) {
+        //alert("Submitting");
         e.preventDefault();
         $.ajax({
             url: '//formspree.io/daniel.delvin.diaz@gmail.com',
             method: 'POST',
-            data: $('#contact-form').serialize(),
+            data: $(this).serialize(),
             dataType: 'json',
             beforeSend: function() {
             },
@@ -16,10 +16,10 @@ $(function(){
                 //$contactForm.find('#contact-form').hide();
                 //$(this).closest('form').find("input[type=text], textarea").val("");
                 $("#contact-submit-btn").hide();
-                $('#contact-form').append('<div class="">Message sent! I will get back to you shortly!</div>');
+                $contactForm.append('<div class="">Message sent! I will get back to you shortly!</div>');
             },
             error: function(err) {
-                $('#contact-form')append('<div class=""> Dang it, there was an error. Make sure you filled out the fields and please try again.</div>');
+                $contactForm.append('<div class=""> Dang it, there was an error. Make sure you filled out the fields and please try again.</div>');
             }
         });
     });
